0623de68c26458c2ee0d1f07d1f8c26957a6cea9
Evgeny Mikheev <eudgee@gmail.com>
2020-01-17 15:34:41 +0300

    config

--- tmux-start.sh
+++ tmux-start.sh
@@ -4,6 +4,6 @@ tmux new-session -d -s tmux
 tmux select-window -t tmux:0
 tmux split-window -h
 tmux split-window -v -t 0
-tmux split-window -v -t 1
+tmux split-window -v -t 2
 tmux attach-session -t tmux
 
